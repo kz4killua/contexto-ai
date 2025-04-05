d13f127a2e3a447acda50ae01fda243b19b78090
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-04-04 23:05:10 -0400

    Add get_rank() method to Game class

--- game.py
+++ game.py
@@ -11,9 +11,11 @@ class Game:
 
     def make_guess(self, word):
         if word not in self.history:
-            rank = get_rank(word, self.number)
-            self.history[word] = rank
+            self.history[word] = self.get_rank(word)
         return self.history[word]
+    
+    def get_rank(self, word):
+        return get_rank(word, self.number)
     
     @property
     def number_of_guesses(self):
